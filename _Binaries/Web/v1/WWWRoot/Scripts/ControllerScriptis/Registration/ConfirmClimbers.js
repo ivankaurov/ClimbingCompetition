b7636e6ba3900dbb412573c54cd50bf6fbf63595
IvanK (LapTop) <ivan.kaurov@gmail.com>
2016-10-27 23:27:36 +0300

    Binaries remove

--- _Binaries/Web/v1/WWWRoot/Scripts/ControllerScriptis/Registration/ConfirmClimbers.js
+++
@@ -1,9 +0,0 @@
-﻿function btnSubmit_Click() {
-    $("#goback").val("false");
-    $(".hide-when-edit").hide();
-}
-
-function btnBack_Click() {
-    $("#goback").val("true");
-    $(".hide-when-edit").hide();
-}
