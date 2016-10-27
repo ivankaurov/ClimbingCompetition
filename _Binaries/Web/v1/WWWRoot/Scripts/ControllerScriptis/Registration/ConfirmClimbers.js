f872b3d3c74e4f70bdac416d33252637368ae908
IvanK (LapTop) <ivan.kaurov@gmail.com>
2016-10-27 22:56:52 +0300

    Binaries

---
+++ _Binaries/Web/v1/WWWRoot/Scripts/ControllerScriptis/Registration/ConfirmClimbers.js
@@ -0,0 +1,9 @@
+﻿function btnSubmit_Click() {
+    $("#goback").val("false");
+    $(".hide-when-edit").hide();
+}
+
+function btnBack_Click() {
+    $("#goback").val("true");
+    $(".hide-when-edit").hide();
+}
---
+++ _Binaries/Web/v1/WWWRoot/Scripts/ControllerScriptis/Registration/NewClimber.js
@@ -0,0 +1,41 @@
+﻿function btnSubmit_Click() {
+    if (confirm("Подать заявку?")) {
+        SetLoadingState(true, false);
+        $("#frm_ClimbersList").submit();
+    }
+}
+
+function lnkNewClimber_Click() {
+    var indx = parseInt($("#frm_NewClimber_Index").val());
+    indx = indx + 1;
+    $("#frm_NewClimber_Index").val(indx);
+    SetLoadingState(true, true);
+    $("#frm_NewClimber").submit();
+}
+
+function lnkNewClimber_Failed() {
+    alert("Ошибка загрузки формы для нового участника");
+}
+
+function lnkNewClimber_Completed() {
+    SetLoadingState(false);
+}
+
+function SetLoadingState(loading, disabled) {
+    if (loading) {
+        $('#div_loader').show();
+        $(".hide-when-edit").hide();
+        if (disabled) {
+            $(".frm-editor").attr("disabled", "disabled");
+        }
+        else {
+            $(".frm-editor").attr("readonly", "readonly");
+        }
+    }
+    else {
+        $('#div_loader').hide();
+        $(".hide-when-edit").show();
+        $(".frm-editor").removeAttr("disabled");
+        $(".frm-editor").removeAttr("readonly");
+    }
+}
