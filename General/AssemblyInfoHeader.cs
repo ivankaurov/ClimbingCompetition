69ae485e29e215320d81d0d0f90e810882cca70a
Ivan Kaurov <ivan.kaurov@gmail.com>
2018-02-10 02:40:31 +0300

    Installer without shortcuts

--- General/AssemblyInfoHeader.cs
+++ General/AssemblyInfoHeader.cs
@@ -63,6 +63,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.2.10.1")]
-[assembly: AssemblyFileVersion("2018.2.10.1")]
+[assembly: AssemblyVersion("18.2.10.1")]
+[assembly: AssemblyFileVersion("18.2.10.1")]
 [assembly: NeutralResourcesLanguage("ru")]
