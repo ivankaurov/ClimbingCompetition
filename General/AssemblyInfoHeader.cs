1fedf8bd957eb24f2f091193dee389c6c340aef1
Ivan Kaurov <ivan.kaurov@gmail.com>
2018-03-18 23:43:07 +0300

    18.3 release version

--- General/AssemblyInfoHeader.cs
+++ General/AssemblyInfoHeader.cs
@@ -62,8 +62,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.3.16.1")]
-[assembly: AssemblyFileVersion("18.3.16.1")]
+[assembly: AssemblyVersion("18.3.18.1")]
+[assembly: AssemblyFileVersion("18.3.18.1")]
 [assembly: NeutralResourcesLanguage("ru")]
-[assembly: AssemblyInformationalVersion("18.3-rc1")]
+[assembly: AssemblyInformationalVersion("18.3")]
 [assembly: AssemblyDescription("Новые версии приложения тут: https://github.com/ivankaurov/ClimbingCompetition/releases")]
