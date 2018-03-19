89f53bf057a1d4cfb27894239deba0134d0f4612
Ivan Kaurov <ivan.kaurov@gmail.com>
2018-03-19 19:47:13 +0300

    UNCOMPILABLE: New list form

---
+++ Desktop/UserForms/CombinedNewList.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ClimbingCompetition
+{
+    partial class CombinedNewList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbNewList = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreateList = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbNewList
+            // 
+            this.rbNewList.AutoSize = true;
+            this.rbNewList.Location = new System.Drawing.Point(13, 13);
+            this.rbNewList.Name = "rbNewList";
+            this.rbNewList.Size = new System.Drawing.Size(152, 17);
+            this.rbNewList.TabIndex = 0;
+            this.rbNewList.TabStop = true;
+            this.rbNewList.Text = "Создать новый протокол";
+            this.rbNewList.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 37);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(189, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "На основе текущих результатов";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "При выборе этого пункта убедитесь,";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(329, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "что итоговые протоколы созданы и не непроведенных раундов";
+            // 
+            // btnCreateList
+            // 
+            this.btnCreateList.Location = new System.Drawing.Point(13, 98);
+            this.btnCreateList.Name = "btnCreateList";
+            this.btnCreateList.Size = new System.Drawing.Size(82, 39);
+            this.btnCreateList.TabIndex = 4;
+            this.btnCreateList.Text = "Создать протокол";
+            this.btnCreateList.UseVisualStyleBackColor = true;
+            this.btnCreateList.Click += new System.EventHandler(this.btnCreateList_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(268, 98);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 39);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // CombinedNewList
+            // 
+            this.AcceptButton = this.btnCreateList;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(362, 143);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCreateList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.rbNewList);
+            this.Name = "CombinedNewList";
+            this.Text = "Создать протокол многоборья";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbNewList;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreateList;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Desktop/UserForms/CombinedNewList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClimbingCompetition
+{
+    public partial class CombinedNewList : Form
+    {
+        public enum CombinedNewListType
+        {
+            NewList,
+        }
+
+        public CombinedNewList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateList_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
