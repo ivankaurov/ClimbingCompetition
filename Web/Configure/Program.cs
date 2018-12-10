3d30b3b19ce2414f84cc793451768c828d25d49c
Ivan Kaurov <ivan.kaurov@gmail.com>
2018-12-11 00:13:11 +0300

    Configure project added

---
+++ Web/Configure/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClimbingCompetition.Web.Configure
+{
+    using System;
+    using System.Threading.Tasks;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            try
+            {
+                Console.WriteLine("Configuring DB");
+                await Task.CompletedTask;
+                Console.WriteLine("Configure completed!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Configure failed: {ex}");
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
